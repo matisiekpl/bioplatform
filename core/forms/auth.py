b004c84efc07d2459c253b0ca20644cb39115c83
Mateusz Woźniak <matisiek11@gmail.com>
2025-06-04 12:32:33 +0200

    fix: password hash

--- core/forms/auth.py
+++ core/forms/auth.py
@@ -21,4 +21,11 @@ class UserRegistrationForm(forms.ModelForm):
         if password and confirm_password and password != confirm_password:
             raise forms.ValidationError("Hasła nie pasują do siebie")
 
-        return cleaned_data 
+        return cleaned_data
+        
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password"])
+        if commit:
+            user.save()
+        return user
